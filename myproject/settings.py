96b0d472ff2f77973f6df766eb53aa78b39fd325
Radin Khansari <Radinkhansari@gmail.com>
2026-08-09 22:45:03 -0400

    Add banner images to posts and configure media files

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
