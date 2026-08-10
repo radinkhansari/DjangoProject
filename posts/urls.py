507433203f6a8f37103e8f2bce8e1f8dd9f2936a
Radin Khansari <Radinkhansari@gmail.com>
2026-08-10 12:18:26 -0400

    Add new post route and auth-aware navigation

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name = 'list'),
+    path('new-post/', views.post_new, name = 'new_post'),
     path('<slug:slug>', views.post_page, name = 'page'),
 ]
